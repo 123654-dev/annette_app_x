3baeafbd5c6fbba7ccd65fd06d5055e59b48b371
123654-dev <72887184+123654-dev@users.noreply.github.com>
2023-05-16 22:23:01 +0200

    id-Bug gefixt (und es ist kryptografisch fast sicher)

--- lib/providers/notifications.dart
+++ lib/providers/notifications.dart
@@ -1,5 +1,6 @@
 import 'package:annette_app_x/utilities/homework_manager.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:nanoid/nanoid.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
@@ -17,7 +18,7 @@ class NotificationProvider {
         requestAlertPermission: false,
         onDidReceiveLocalNotification:
             (int id, String? title, String? body, String? payload) async {
-          print('Received notification with id $id');
+          print('📘 | Received notification with id $id [notifications.dart]');
         });
 
     var initializationSettings = InitializationSettings(
@@ -26,7 +27,8 @@ class NotificationProvider {
     );
     await notificationsPlugin.initialize(initializationSettings,
         onDidReceiveNotificationResponse: (payload) async {
-      print('Received notification with payload: $payload');
+      print(
+          '📘 | Received notification action with payload: $payload [notifications.dart]');
     });
   }
 
@@ -43,6 +45,7 @@ class NotificationProvider {
   }
 
   Future<void> cancelNotification(int id) async {
+    print("🚫 | CANCELLING notification with id $id [notifications.dart]");
     await notificationsPlugin.cancel(id);
   }
 
@@ -52,8 +55,8 @@ class NotificationProvider {
       String? title,
       String? body,
       String? payload}) async {
-    var id = HomeworkManager.howManyEntries();
-    print("Scheduling notification with id $id");
+    var id = int.parse(customAlphabet('1234567890', 9));
+    print("📗 | Scheduling notification with id $id [notifications.dart]");
     await notificationsPlugin.zonedSchedule(id, title, body,
         tz.TZDateTime.from(date, tz.local), makeNotificationDetails(),
         androidScheduleMode: AndroidScheduleMode.exact,
