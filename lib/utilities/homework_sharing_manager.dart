1af2f4993ec0be72d6746d758d7263415445164f
Eli-dd <90352951+Eli-dd@users.noreply.github.com>
2023-08-13 15:14:05 +0200

    file conversion und sharing implementiert
    (Man kann die HAs noch nicht teilen, aber Maps könnten schon intern geteilt werden)

---
+++ lib/utilities/homework_sharing_manager.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:annette_app_x/providers/api/files_provider.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:cross_file/src/types/interface.dart';
+
+import '../models/file_format.dart';
+
+class HomeworkSharer{
+  static Future<void> shareHomework(Map<String, dynamic> homeworkData) async {
+    //Share the homework as a file with the extension .homework
+    List<int> bytes = convertMapToBytes(homeworkData);
+    //Share the file
+    Share.shareXFiles([XFile.fromData(Uint8List.fromList(bytes))], text: 'test');
+  }
+
+  static List<int> convertMapToBytes(Map<String, dynamic> map){
+    String json = jsonEncode(map);
+    List<int> bytes = utf8.encode(json);
+    return bytes;
+  }
+
+  static Map<String, dynamic> convertBytesToMap(List<int> bytes){
+    String json = utf8.decode(bytes);
+    Map<String, dynamic> map = jsonDecode(json);
+    return map;
+  }
+}
