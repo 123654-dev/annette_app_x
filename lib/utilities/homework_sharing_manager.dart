f2176e37b7c6c2a35cc29ecea5cdb0794c179605
Eli-dd <90352951+Eli-dd@users.noreply.github.com>
2023-08-13 15:20:15 +0200

    Unterstützung für HomeWorkEntry Objekte hinzugefügt

--- lib/utilities/homework_sharing_manager.dart
+++ lib/utilities/homework_sharing_manager.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:annette_app_x/models/homework_entry.dart';
 import 'package:annette_app_x/providers/api/files_provider.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:cross_file/src/types/interface.dart';
@@ -8,15 +9,21 @@ import 'package:cross_file/src/types/interface.dart';
 import '../models/file_format.dart';
 
 class HomeworkSharer{
-  static Future<void> shareHomework(Map<String, dynamic> homeworkData) async {
+  static Future<void> shareHomework(HomeworkEntry entry) async {
     //Share the homework as a file with the extension .homework
-    List<int> bytes = convertMapToBytes(homeworkData);
+    List<int> bytes = convertHomeworkToBytes(entry);
     //Share the file
     Share.shareXFiles([XFile.fromData(Uint8List.fromList(bytes))], text: 'test');
   }
 
   static List<int> convertMapToBytes(Map<String, dynamic> map){
     String json = jsonEncode(map);
+    List<int> bytes = utf8.encode(json);
+    return bytes;
+  }
+
+  static List<int> convertHomeworkToBytes(HomeworkEntry entry){
+    String json = jsonEncode(entry);
     List<int> bytes = utf8.encode(json);
     return bytes;
   }
