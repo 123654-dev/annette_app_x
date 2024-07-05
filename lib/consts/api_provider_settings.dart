db7a11238fa42f249e5ca7a39e73055267ae8bbf
123654-dev <72887184+123654-dev@users.noreply.github.com>
2024-07-05 16:58:44 +0200

    Ferienerkennung implementiert
    An Ferientagen wird eine entsprechende Info angezeigt.

--- lib/consts/api_provider_settings.dart
+++ lib/consts/api_provider_settings.dart
@@ -3,9 +3,5 @@ class ApiProviderSettings {
   ///Allgemeiner URL-Präfix der API
   ///Standardmäßig annette-api-x.vercel.app.
   ///Kann zum Testen von nicht-prod-Branches der API geändert werden.
-<<<<<<< Updated upstream
   static const String baseURL = "annette-api-x.vercel.app";
-=======
-  static const String baseURL = "10.20.5.178:3000";
->>>>>>> Stashed changes
 }
